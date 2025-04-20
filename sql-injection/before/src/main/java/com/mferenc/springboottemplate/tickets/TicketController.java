7ce9b7c24069581d5c40c3df1e26a83beab7bcec
Maciej Ferenc <maciekferenc1@gmail.com>
2025-04-20 22:17:45 +0200

    refactor retrieving authenticated user's id

--- sql-injection/before/src/main/java/com/mferenc/springboottemplate/tickets/TicketController.java
+++ sql-injection/before/src/main/java/com/mferenc/springboottemplate/tickets/TicketController.java
@@ -1,12 +1,10 @@
 package com.mferenc.springboottemplate.tickets;
 
+import com.mferenc.springboottemplate.auth.AuthenticationFacade;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.Query;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,23 +15,20 @@ import java.util.List;
 @RestController
 @RequestMapping("/tickets")
 public class TicketController {
+    private final AuthenticationFacade auth;
 
     @PersistenceContext
     private EntityManager entityManager;
+
+    public TicketController(AuthenticationFacade auth) {
+        this.auth = auth;
+    }
 
     @GetMapping
     public ResponseEntity<List<Ticket>> getTickets(
             @RequestParam(required = false) String phrase
     ) {
-
-        Authentication authentication = SecurityContextHolder
-                .getContext()
-                .getAuthentication();
-        String username = ((UserDetails) authentication.getPrincipal()).getUsername();
-
-        String userIdSql = "SELECT id FROM users WHERE username ='" + username + "'";
-        Query userQuery = entityManager.createNativeQuery(userIdSql);
-        Long userId = ((Number) userQuery.getSingleResult()).longValue();
+        long userId = auth.getCurrentUser().getId();
 
         String sql = "SELECT * FROM tickets WHERE user_id = " + userId;
 
